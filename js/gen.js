11de8adc8b36e437b051edae4d23c0060d610786
sopoat0101 <60070101@kmitl.ac.th>
2018-11-08 00:36:47 +0700

    Update generation page add infomation and picture.

--- js/gen.js
+++ js/gen.js
@@ -1,6 +1,5 @@
-var url = ["../data/renaissance.json", "../data/renaissance.json", "../data/renaissance.json", "../data/renaissance.json", "../data/renaissance.json"];
-
 var DATA;
+var INFO;
 
 window.onload = connectJson();
 
@@ -20,30 +19,42 @@ function connectJson(){
         }
     };
 
-    xmlhttp.open("GET", url[index], true);
+    xmlhttp.open("GET", "../data/gen.json", true);
     xmlhttp.send();
 
 }
 
 function loadPage(data, index){
-    console.log(data);
-    document.title = data.gen.nameth;
+    // console.log(data);
+    document.title = data.gen[index].nameth;
     document.getElementsByClassName('menuitem')[parseInt(index)+1].setAttribute("style", "color:black");
-    document.getElementsByClassName('head')[0].innerHTML = data.gen.nameth;
+    document.getElementsByClassName('head')[0].innerHTML = data.gen[index].nameth;
 
-    for(var i = 0; i < data.gen.person.length ;i++){
+    readTextFile(data.gen[index].text);
+
+    document.getElementsByClassName('inner')[0].innerHTML = INFO;
+    // gen img
+    for(var i = 0; i < data.gen[index].img.length ;i++){
+        document.getElementsByClassName('pic')[0].innerHTML+= '<div class="img"></div>';
+
+        document.getElementsByClassName('img')[i].style.backgroundImage = 'url('+ data.gen[index].img[i] +')';
+
+    }
+    // gen card
+    for(var i = 0; i < data.gen[index].person.length ;i++){
+
         // console.log(i);
         document.getElementsByClassName('maincard')[0].innerHTML += '\
-        <div class="card">\
+        <div onclick="goto('+ i + ','+ index +')" class="card">\
             <div class="front"></div>\
             <div class="back">\
                 <div class="cardimg"></div>\
                 <div class="tag">Leonardo da Vinci</div>\
             </div>\
         </div>' ;
-        document.getElementsByClassName('tag')[i].innerHTML = data.gen.person[i].name;
-        document.getElementsByClassName('front')[i].style.backgroundImage = "url("+ data.gen.person[i].img +")";
-        document.getElementsByClassName('cardimg')[i].style.backgroundImage = "url("+ data.gen.person[i].img +")";
+        document.getElementsByClassName('tag')[i].innerHTML = data.gen[index].person[i].name;
+        document.getElementsByClassName('front')[i].style.backgroundImage = "url("+ data.gen[index].person[i].img +")";
+        // document.getElementsByClassName('cardimg')[i].style.backgroundImage = "url("+ data.gen[index].person[i].img +")";
 
     }
 
@@ -61,5 +72,35 @@ function getURLdata(){
 
 function setData(data){
     this.DATA = data;
+
+}
+
+function setINFO(info){
+    this.INFO = info;
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                setINFO(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function goto(num, index){
+
+    var myurl = 'person.html' + '?name=' + num+'?index='+ index;
+
+    window.location.assign(myurl);
 
 }
